8b019c06153a10526c8c483b07c3d9c48b4c6484
umesh gupta <umeshg2059@gmail.com>
2026-04-26 21:58:51 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+         <h1 className="text-4xl font-bold text-black-500">
+                Tailwind is Working 🚀
+          </h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
