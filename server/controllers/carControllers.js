a04d200ce4b8ccf5e0b69f8378c0ba0f1769549d
umesh gupta <umeshg2059@gmail.com>
2026-05-05 14:05:54 +0530

    checking updatee method

--- server/controllers/carControllers.js
+++ server/controllers/carControllers.js
@@ -37,3 +37,56 @@ export const getCars = async(req,res)=>{
         })
     }
 }
+
+export const getCarById = async(req,res)=>{
+    try{
+        const car = await Car.findById(req.params.id);
+
+        if(!car){
+            return res.status(404).json({
+                success:false,
+                message:'car not found'
+            });
+        }
+        res.status(200).json({
+            success:true,
+            data:car
+        })
+    }catch(error){
+        res.status(500).json({
+            success:false,
+message:error.message
+        });
+    }
+
+};
+
+export const updateCar = async(req,res)=>{
+    try{
+        const car = await Car.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new:true,       //retured updated car
+                runValidators:true
+            }
+        );
+       if(!car){
+        return res.status(404)({
+            success:false,
+            message:'car not found'
+        })
+
+       }
+       res.status(200).json({
+        success:true,
+        data:car
+       })
+    }catch(error) {
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+    }
+    
+};
--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { createCar,getCars } from '../controllers/carControllers.js';
+import { createCar,getCars,getCarById,updateCar } from '../controllers/carControllers.js';
 
 const router = express.Router();
 
 router.post('/',createCar)
 router.get('/',getCars)
+router.get('/:id',getCarById)
+router.put('/',updateCar)
+
 
 export default router;
 
