cadf8c4efecb6ba7bec6fb412d0044ffa4c3244c
umesh gupta <umeshg2059@gmail.com>
2026-05-12 09:54:04 +0530

    updated ui

--- server/routes/authroutes.js
+++ server/routes/authroutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import { registerUser } from '../controllers/authControllers.js';
+import { loginUser, registerUser } from '../controllers/authControllers.js';
+import { loginUser } from '../controllers/authControllers.js';
 
 const router = express.Router();
 
 
 router.post('/register',registerUser);
+router.post('/login',loginUser)
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ app.get('/',(req,res)=>{
 app.use('/api/cars',carRoutes);
 app.use('/api/auth',authroutes);
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
